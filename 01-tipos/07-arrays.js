ecdcdf987f8f9db2aa8a083cc94fd0be9a0b7b9f
Omar Arias <omararias870503@gmail.com>
2026-05-17 16:13:55 -0400

    Agregando cambios a index y 01-tipos

---
+++ 01-tipos/07-arrays.js
@@ -0,0 +1,6 @@
+let animales = ["perro", "gato", "conejo"];
+
+console.log(animales);
+console.log(animales[0]);
+animales[3] = "hamster";
+console.log(animales);
---
+++ 01-tipos/09-argumentos.js
@@ -0,0 +1,8 @@
+function suma(a, b)
+{
+    return a + b;
+}
+
+let resultado = suma(5, 6);
+console.log(resultado); // Imprime 7
+console.log(typeof suma);
