a90caad716ee9ced08bbac4d8b52a99a1857eddb
Omar Arias <omararias870503@gmail.com>
2026-05-17 11:53:39 -0400

    Agregando cambios en index y carpeta 01-tipos

---
+++ 01-tipos/01-variables.js
@@ -0,0 +1,8 @@
+let nombre = "Hola Mundo";// Declaramos una variable llamada "nombre" y le asignamos el valor "Hola Mundo"
+//convension UpperCamelCase
+//let NombreCompleto;// Declaramos nuevamente la variable "NombreCompleto", lo que no causa ningún error, pero su valor sigue siendo "undefined"
+//convension camelCase
+//let nombreCompleto;// Declaramos una variable llamada "nombreCompleto" sin asignarle un valor, por lo que su valor será "undefined"
+//convension snake_case
+//let nombre_completo;// Declaramos una variable llamada "nombre_completo" sin asignarle un valor, por lo que su valor será "undefined"   
+console.log(nombre);// Imprimimos el valor de la variable "nombre" en la consola, lo que mostrará "Hola Mundo"
