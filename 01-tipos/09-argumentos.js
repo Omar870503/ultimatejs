381fe86e5dfb9da02a528d7cf94e09d50a65cc16
Omar Arias <omararias870503@gmail.com>
2026-05-20 16:08:18 -0400

    Agregando archivo argumentos a carpeta 01-tipos

--- 01-tipos/09-argumentos.js
+++ 01-tipos/09-argumentos.js
@@ -1,8 +1,9 @@
 function suma(a, b)
-{
+{   
+    console.log(arguments); // Imprime los argumentos pasados a la función
     return a + b;
 }
 
-let resultado = suma(5, 6);
+let resultado = suma(5, 6, 1, 2, 3); // Llamada a la función con más argumentos de los definidos    
 console.log(resultado); // Imprime 7
 console.log(typeof suma);
