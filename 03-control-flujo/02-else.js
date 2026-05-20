6d7035f452d3e1f3dbd0b68431c40019a4a9e8de
Omar Arias <omararias870503@gmail.com>
2026-05-20 18:42:03 -0400

    Agregando cambios a index y carpeta control flujos

---
+++ 03-control-flujo/02-else.js
@@ -0,0 +1,10 @@
+let edad = 20;
+
+if (edad > 17){
+    console.log("Eres mayor de edad");
+}else if(edad > 13) {
+    console.log("Eres un adolescente");
+}
+else {
+    console.log("Eres menor de edad");
+}
